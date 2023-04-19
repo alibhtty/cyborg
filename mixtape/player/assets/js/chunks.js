bfe4b8948f6c9a2c505b82765494ebb5aa224fcf
alibhtty <77213825+alibhtty@users.noreply.github.com>
2023-04-19 04:20:34 +0200

    18.6

---
+++ mixtape/player/assets/js/chunks.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('#alibhtty015').click(function(){
+       $("#contenidos").load("https://alibhtty.github.io/cyborg/mixtape/player/set/alibhtty/015.html");
+                                 });
+
+    $('#alibhtty014').click(function(){
+       $("#contenidos").load("https://alibhtty.github.io/cyborg/mixtape/player/set/alibhtty/014.html");
+                                 });
+
+    $('#alibhtty013').click(function(){
+       $("#contenidos").load("https://alibhtty.github.io/cyborg/mixtape/player/set/alibhtty/013.html");
+                                 });
+    $('#alibhtty012').click(function(){
+       $("#contenidos").load("./set/alibhtty/012.html");
+                                 });
+    $('#alibhtty012').click(function(){
+       $("#contenidos").load("./set/alibhtty/011.html");
+                                 });
+    $('#alibhtty012').click(function(){
+       $("#contenidos").load("./set/alibhtty/010.html");
+                                 });
+    $('#alibhtty012').click(function(){
+       $("#contenidos").load("./set/alibhtty/009.html");
+                                 });
+    $('#alibhtty012').click(function(){
+       $("#contenidos").load("./set/alibhtty/008.html");
+                                 });
+                            });
