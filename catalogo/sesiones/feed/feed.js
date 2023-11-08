c97cd489815d270be7c199991d86d4ab253371b6
alibhtty <77213825+alibhtty@users.noreply.github.com>
2023-11-08 01:26:27 +0100

    21.7

--- catalogo/sesiones/feed/feed.js
+++ catalogo/sesiones/feed/feed.js
@@ -160,6 +160,12 @@ function handleGesture() {
       // Ir a la imagen siguiente
       nextButton.click();
   }
+  if (swiped.includes('up')) {
+      // Cerrar el modal solo si la aplicación está en modo standalone
+    if (window.matchMedia('(display-mode: standalone)').matches && Math.abs(touchStartY - touchEndY) > 50) {
+      x.click();
+    }
+}
   if (swiped.includes('down')) {
       // Cerrar el modal solo si la aplicación está en modo standalone
       if (window.matchMedia('(display-mode: standalone)').matches && Math.abs(touchStartY - touchEndY) > 50) {
@@ -178,21 +184,24 @@ function handleGesture() {
 
 
 
-/* // Obtén el elemento que contiene el modal
-var modalContainer = document.querySelector('.modal');
-
-// Obtén la imagen dentro del modal
+/* ===== CAPTION ===== */
+// Supongamos que tienes un elemento en tu modal donde quieres mostrar la imagen
 var modalImage = document.querySelector('.modal-content');
 
-// Cuando el usuario hace clic en el modalContainer, cierra el modal
-modalContainer.addEventListener('click', function(event) {
-    // Si el usuario hizo clic en el modalContainer, cierra el modal
-    if (event.target === modalContainer) {
-        x.click();
-    }
-});
+// Y un elemento donde quieres mostrar el figcaption
+var modalCaption = document.querySelector('.modal-caption');
 
-// Cuando el usuario hace clic en la imagen, evita que el evento se propague al modalContainer
-modalImage.addEventListener('click', function(event) {
-    event.stopPropagation();
-}); */
+// Asegúrate de que todas tus imágenes tienen un event listener para el evento de clic
+var images = document.querySelectorAll('.img_card');
+images.forEach(function(img) {
+    img.addEventListener('click', function(event) {
+        // Aquí es donde muestras la imagen en el modal
+        modalImage.src = this.src;
+        
+        // Y aquí es donde puedes agregar el figcaption al modal
+        var figcaption = this.nextElementSibling;
+        if (figcaption) {
+            modalCaption.textContent = figcaption.textContent;
+        }
+    });
+});
